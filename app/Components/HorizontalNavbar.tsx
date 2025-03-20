1b998aca15b3799b2b7c8db225777dc51c476e65
Aayush Agrawal <aayushagrawal97@gmail.com>
2025-03-20 09:36:05 +0530

    StartProject

---
+++ app/Components/HorizontalNavbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div>
+          <ul className="nav nav-pills">
+            <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+    </div>
+  )
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import Navbar from "~/Components/Navbar";
+import Footer from "~/Components/Footer";
+import Accordion from "~/Components/Accordion";
+import Header from "~/Components/Header";
+import PageNotFound from "~/Components/PageNotFound";
+import UpperCarousel from "~/Components/UpperCarousel";
+import VerticalNavbar from "~/Components/VerticalNavbar";
+import HorizontalNavbar from "~/Components/HorizontalNavbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Anmay Academy" },
+    { name: "description", content: "Welcome to Online EdTech Platform!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <HorizontalNavbar />
+      <VerticalNavbar />
+      <UpperCarousel />
+      <PageNotFound />
+      <Accordion />
+      <Footer />
+    </div>
+  );
 }
